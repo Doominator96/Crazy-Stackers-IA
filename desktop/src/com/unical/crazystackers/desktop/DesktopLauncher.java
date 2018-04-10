ae9e2129500feed47c44f093a0a46a721b95c1d8
Domenico Laurito <www.domenico.96@gmail.com>
2018-04-10 16:37:39 +0200

    Initial Commit

---
+++ desktop/src/com/unical/crazystackers/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unical.crazystackers.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unical.crazystackers.CrazyStackers;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrazyStackers(), config);
+	}
+}
